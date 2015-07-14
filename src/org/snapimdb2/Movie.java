153cd250c09450be9e80877f1a86b83de508d75e
Suraj Kumar Sau <i@surajsau.in>
2015-07-14 16:14:57 +0530

    Movie class getters-setters added

--- src/org/snapimdb2/Movie.java
+++ src/org/snapimdb2/Movie.java
@@ -1,7 +1,37 @@
 package org.snapimdb2;
+
+import java.util.ArrayList;
+
 public class Movie {
 	
 	private MovieMetaData movieMetaData;
 	private String movieId;
+	private Rating rating;
+	private ArrayList<Review> reviews;
+	public MovieMetaData getMovieMetaData() {
+		return movieMetaData;
+	}
+	public void setMovieMetaData(MovieMetaData movieMetaData) {
+		this.movieMetaData = movieMetaData;
+	}
+	public String getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+	public Rating getRating() {
+		return rating;
+	}
+	public void setRating(Rating rating) {
+		this.rating = rating;
+	}
+	public ArrayList<Review> getReviews() {
+		return reviews;
+	}
+	public void setReviews(ArrayList<Review> reviews) {
+		this.reviews = reviews;
+	}
+
 
 }
