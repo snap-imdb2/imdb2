a46bfbce14b203648e36983540da171609c5e333
Tanuj Rastogi <r.tanuj@gmail.com>
2015-07-14 13:10:48 +0530

    Rating class implemented

--- src/org/snapimdb2/Rating.java
+++ src/org/snapimdb2/Rating.java
@@ -1,4 +1,20 @@
 package org.snapimdb2;
 public class Rating {
-
+    
+    private long totalNumberOfRatings;
+    private long totalSumOfRatings;
+    
+    public Rating() {
+        totalNumberOfRatings = 0;
+        totalSumOfRatings = 0;
+    }
+    
+    public double getRating(){
+        return (((double)totalSumOfRatings)/totalNumberOfRatings);
+    }
+    
+    public void addRating(int rating){
+        totalNumberOfRatings++;
+        totalSumOfRatings+=rating;
+    }
 }
