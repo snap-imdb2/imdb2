fb4d06c2f7efa5a62056b6051e498a7a45ba371f
Suraj Kumar Sau <i@surajsau.in>
2015-07-15 14:45:55 +0530

    added getter-setters in UserMetaData

--- src/org/snapimdb2/meta/UserMetaData.java
+++ src/org/snapimdb2/meta/UserMetaData.java
@@ -1,5 +1,40 @@
 package org.snapimdb2.meta;
-public class UserMetaData {
-	String userName;
 
+import java.util.Calendar;
+
+public class UserMetaData {
+	private String username;
+	private String password;
+	private Calendar dateOfJoining;
+	
+	public UserMetaData(String username, String password, Calendar dateOfJoining){
+		this.username = username;
+		this.password = password;
+		this.dateOfJoining = dateOfJoining;
+	}
+	
+	public Calendar getDateOfJoining() {
+		return dateOfJoining;
+	}
+	
+	public void setDateOfJoining(Calendar dateOfJoining) {
+		this.dateOfJoining = dateOfJoining;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+	
 }
