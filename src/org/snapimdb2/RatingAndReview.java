4619fa9d1c9b4de95472ac1016f49792186bae02
Suraj Kumar Sau <i@surajsau.in>
2015-07-16 10:34:12 +0530

    User & RatingAndReview getters-setters implemented

--- src/org/snapimdb2/RatingAndReview.java
+++ src/org/snapimdb2/RatingAndReview.java
@@ -1,4 +1,43 @@
 package org.snapimdb2;
-public class RatingAndReview {
 
+import org.snapimdb2.meta.ReviewMetaData;
+
+public class RatingAndReview {
+	private String reviewId;
+	private String userId;
+	private String movieId;
+	private ReviewMetaData reviewMetaData;
+	
+	public String getReviewId() {
+		return reviewId;
+	}
+	
+	public void setReviewId(String reviewId) {
+		this.reviewId = reviewId;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	public String getMovieId() {
+		return movieId;
+	}
+	
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+	
+	public ReviewMetaData getReviewMetaData() {
+		return reviewMetaData;
+	}
+	
+	public void setReviewMetaData(ReviewMetaData reviewMetaData) {
+		this.reviewMetaData = reviewMetaData;
+	}
+	
 }
--- src/org/snapimdb2/User.java
+++ src/org/snapimdb2/User.java
@@ -1,4 +1,45 @@
 package org.snapimdb2;
-public class User {
 
+import java.util.ArrayList;
+
+import org.snapimdb2.meta.UserMetaData;
+
+public class User {
+	private String userId;
+	private ArrayList<RatingAndReview> listOfRatingsAndReviews;
+	private UserMetaData userMetaData;
+	private boolean isAdmin;
+	
+	public String getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	public ArrayList<RatingAndReview> getListOfRatingsAndReviews() {
+		return listOfRatingsAndReviews;
+	}
+	
+	public void setListOfRatingsAndReviews(ArrayList<RatingAndReview> listOfRatingsAndReviews) {
+		this.listOfRatingsAndReviews = listOfRatingsAndReviews;
+	}
+	
+	public UserMetaData getUserMetaData() {
+		return userMetaData;
+	}
+	
+	public void setUserMetaData(UserMetaData userMetaData) {
+		this.userMetaData = userMetaData;
+	}
+	
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	
 }
