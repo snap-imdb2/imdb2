dca622cc46af81cc586aeaff3f56b9a20a84a73b
Suraj Kumar Sau <i@surajsau.in>
2015-07-17 10:52:02 +0530

    removed lists from User, Review class & updated schema_setup with user, review tables

--- scripts/schema_setup.sql
+++ scripts/schema_setup.sql
@@ -9,3 +9,20 @@ numberOfRatings INT,
 sumOfRatings INT,
 description TEXT
 );
+
+create table user (
+user_id INT UNSIGNED primary key,
+userName varchar(50),
+email varchar(100),
+password varchar(30),
+dateOfJoining DATE,
+);
+
+create table review (
+user_id INT UNSIGNED NOT NULL,
+movie_id INT UNSIGNED NOT NULL,
+rating INT,
+review TEXT,
+reviewDateTime DATE,
+primary key(movie_id, user_id)
+);
